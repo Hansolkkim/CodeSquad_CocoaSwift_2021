6cc8e0be2b733f2c6c43753ca865f707e31f1d7e
sol <hskim6521@naver.com>
2021-11-09 18:52:17 +0900

    Week2_TUE - Day1때 만들었던 여러 단위 변환 함수를 활용한 추가 미션 Solution 추가

--- Week2_MeasureApp/Week2_MeasureApp/ViewController.swift
+++ Week2_MeasureApp/Week2_MeasureApp/ViewController.swift
@@ -11,21 +11,76 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var unconvertedValue: UITextField!
     @IBOutlet weak var convertedValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     @IBAction func typingCmValue(_ sender: Any) {
-//        let unconveredVal = sender
     }
     @IBAction func convertButton(_ sender: UIButton) {
         
-        let unconvertedVal = Measurement(value: Double(unconvertedValue.text!) ?? 0.0, unit: UnitLength.centimeters)
-        let convertedVal = unconvertedVal.converted(to: UnitLength.inches)
-        convertedValue.text = String(convertedVal.value) //Measurement를 사용하면 데이터 타입이 Measurement로 정해져서 그 값을 변환하기 어려웠음.
-                                                     //String으로 변환하기 위해, convertedVal의 값만을 사용하는 convertedVal.value를 String으로 변환했음.
+        let str = unconvertedValue.text!
+        var cntblank = 0
         
+        if str.contains(" ") {
+            cntblank = cntblank + 1
+        }
+        if cntblank == 0 {
+            
+            if str.contains("cm") {
+                convertedValue.text = String(describing: ((str as NSString).floatValue / 100)) + "m"
+            }else if str.contains("inch") {
+                convertedValue.text = String(describing: (convertToCM(fromInches: (str as NSString).floatValue))) + "cm"
+            }else if (str.contains("m")) && !(str.contains("c")) {
+                convertedValue.text =  String(describing: ((str as NSString).floatValue * 100)) +  "cm"
+            }
+            
+        } else {
+            
+            let devidedStr = str.split(separator: " ")
+            var target : String = ""
+            let characterSet = "abcdefghijklmnopqrstuvwxyz"
+            for char in devidedStr[0] {
+                
+                if characterSet.contains(char) {
+                    
+                    target.append(char)
+                }
+            }
+            
+            switch (target, devidedStr[1]) {
+                
+            case("cm", "inch") :
+                convertedValue.text = String(describing: (convertToInches(fromCM: (devidedStr[0] as NSString).floatValue))) + "inch"
+            case("m", "inch") :
+                convertedValue.text = String(describing: (convertToInches(fromCM: (devidedStr[0] as NSString).floatValue * 100))) + "inch"
+            case("inch", "cm") :
+                convertedValue.text = String(describing: (convertToCM(fromInches: (devidedStr[0] as NSString).floatValue))) + "cm"
+            case("inch", "m") :
+                convertedValue.text = String(describing: (convertToCM(fromInches: (devidedStr[0] as NSString).floatValue) / 100)) + "m"
+            case("cm", "m") :
+                convertedValue.text = String(describing: ((devidedStr[0] as NSString).floatValue / 100)) + "m"
+            case("m", "cm") :
+                convertedValue.text = String(describing: ((devidedStr[0] as NSString).floatValue * 100)) + "cm"
+            default:
+                convertedValue.text =  "Error"
+            }
+        }
     }
+    
+    func convertToInches(fromCM: Float) -> Float {
+        var result: Float = 0
+        result = fromCM / 2.54
+        return result
+    }
+    func convertToCM(fromInches : Float) -> Float { //inch - > cm converter function
+        var result : Float = 0
+        result = fromInches * 2.54
+        return result
+    }
+    
+    
     
 }
 
