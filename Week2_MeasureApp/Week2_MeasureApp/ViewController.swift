fc14611974d91f8f8fbae8564be6f6dae154ad71
sol <hskim6521@naver.com>
2021-11-09 17:28:29 +0900

    Week2_TUE - Day1때 만들었던 cm->Inch 변환 함수 사용한 Solution

--- Week2_MeasureApp/Week2_MeasureApp/ViewController.swift
+++ Week2_MeasureApp/Week2_MeasureApp/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var unconvertedValue: UITextField!
+    @IBOutlet weak var convertedValue: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func typingCmValue(_ sender: Any) {
+//        let unconveredVal = sender
+    }
+    @IBAction func convertButton(_ sender: UIButton) {
+        let unconvertedVal = Float(unconvertedValue.text!) ?? 0
+        let convertedVal = convertToInches(fromCM: unconvertedVal)
+        convertedValue.text = String(convertedVal)
+    }
+    
+    func convertToInches(fromCM: Float) -> Float {
+        var result: Float = 0
+        result = fromCM / 2.54
+        return result
+    }
+    
 }
 
