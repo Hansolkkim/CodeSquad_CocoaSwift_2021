11f8d329a71a91d9ca6e91d276af5fa0da30a78f
sol <hskim6521@naver.com>
2021-11-10 15:11:02 +0900

    Week2_Wed 연습문제3 Solution 추가

--- enterPasswordApp/enterPasswordApp/ViewController.swift
+++ enterPasswordApp/enterPasswordApp/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var levelViewLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        titleLabel.text = "Password"
+        descriptionLabel.text = "암호 수준"
+        
     }
-
-
+    @IBAction func checkButton(_ sender: Any) {
+        let passwordString = passwordInput.text!
+        
+        let myValidator = validator()
+        let mylevel = myValidator.passwordValidator(password: passwordString)
+        
+        switch mylevel {
+        case 1 :
+            levelViewLabel.backgroundColor = UIColor.red
+        case 2 :
+            levelViewLabel.backgroundColor = UIColor.orange
+        case 3 :
+            levelViewLabel.backgroundColor = UIColor.yellow
+        case 4 :
+            levelViewLabel.backgroundColor = UIColor.blue
+        case 5 :
+            levelViewLabel.backgroundColor = UIColor.green
+        default :
+            levelViewLabel.backgroundColor = UIColor.black
+        }
+    }
+    
+    
 }
 
---
+++ enterPasswordApp/enterPasswordApp/validator.swift
@@ -0,0 +1,59 @@
+//
+//  validator.swift
+//  enterPasswordApp
+//
+//  Created by 김한솔 on 2021/11/10.
+//
+
+import Foundation
+
+struct validator {
+    
+    func passwordValidator(password: String) -> Int {
+        
+        var grade = 0
+        let length = password.count
+        let numberCharacterset: CharacterSet = CharacterSet.decimalDigits
+        let letterCharacterset: CharacterSet = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        let symbolCharacterset: CharacterSet = CharacterSet(charactersIn: "-,+,!,@,#,$,%")
+        let numberAndLetterCharacterset: CharacterSet = numberCharacterset.union(letterCharacterset)
+        let passwordSet = CharacterSet(charactersIn: password)
+
+        if length < 8 { //password 길이가 8글자 미만
+            
+            if passwordSet.intersection(numberAndLetterCharacterset) != [] {
+                grade += 1
+                if passwordSet.intersection(letterCharacterset) != [] {
+                    grade += 1
+                    if passwordSet.intersection(symbolCharacterset) != [] {
+                        grade += 1
+                    }
+                } else if passwordSet.intersection(symbolCharacterset) != [] {
+                    grade += 2
+                }
+            } else if passwordSet.intersection(symbolCharacterset) != [] {
+                grade = 3
+            }
+            
+        } else if length >= 8 { //password 길이가 8글자 이상
+            grade = 2 // 최소 2 Level
+            
+            if passwordSet.intersection(numberAndLetterCharacterset) != [] {
+                grade += 1
+                if passwordSet.intersection(letterCharacterset) != [] && passwordSet.intersection(numberCharacterset) != [] {
+                    grade += 1
+                    if passwordSet.intersection(symbolCharacterset) != [] {
+                        grade += 1
+                    }
+                } else if passwordSet.intersection(symbolCharacterset) != [] {
+                    grade = 3
+                }
+                
+            }
+        }
+        
+        return grade
+    }
+    
+    
+}
