614f5b597384abbec12735c58f6aad7147b007c1
sol <hskim6521@naver.com>
2021-11-10 10:36:23 +0900

    Week2_WED - 연습문제1,2 파일 생성

---
+++ Week2_WedEx_1_2/Week2_WedEx_1_2/interestRate.swift
@@ -0,0 +1,8 @@
+//
+//  interestRate.swift
+//  Week2_WedEx_1_2
+//
+//  Created by 김한솔 on 2021/11/10.
+//
+
+import Foundation
---
+++ Week2_WedEx_1_2/Week2_WedEx_1_2/numberBaseballGame.swift
@@ -0,0 +1,8 @@
+//
+//  numberBaseballGame.swift
+//  Week2_WedEx_1_2
+//
+//  Created by 김한솔 on 2021/11/10.
+//
+
+import Foundation
