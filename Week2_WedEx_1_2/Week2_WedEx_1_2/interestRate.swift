111329bd0b0b47595dd54786f7ef254dcaf260d8
sol <hskim6521@naver.com>
2021-11-10 11:13:07 +0900

    Week2_WED - 연습문제1 Solution 추가

--- Week2_WedEx_1_2/Week2_WedEx_1_2/interestRate.swift
+++ Week2_WedEx_1_2/Week2_WedEx_1_2/interestRate.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+struct interestRate {
+    
+    func getInterestRate(byDay: Int) -> Double {
+        
+        var InterestRate: Double
+        
+        switch byDay {
+            
+        case 0...90 :
+            InterestRate = 0.5 / 100
+        case 91...180 :
+            InterestRate = 1 / 100
+        case 181...364 :
+            InterestRate = 2 / 100
+        case 365... :
+            InterestRate = 5.6 / 100
+        default :
+            InterestRate = 0.0
+        }
+        
+        return InterestRate
+    }
+    
+    func calculateAmount(day: Int, amount: Int) -> Double {
+        
+        var calculatedAmount: Double
+        let myInterestRate = getInterestRate(byDay: day)
+        
+        calculatedAmount = Double(amount) + (Double(amount) * myInterestRate)
+        
+        return calculatedAmount
+    }
+    
+}
--- Week2_WedEx_1_2/Week2_WedEx_1_2/main.swift
+++ Week2_WedEx_1_2/Week2_WedEx_1_2/main.swift
@@ -7,5 +7,33 @@
 
 import Foundation
 
-print("Hello, World!")
+func testInterest(unitDay: Int) -> Double {
+    
+    let myAmount:Double
+    let testInterestRate = interestRate()
+    
+    myAmount = testInterestRate.calculateAmount(day: unitDay, amount: 1000000)
+    
+    return myAmount
+}
 
+var someInterestRate = interestRate()
+
+print("당신의 예금 거치 기간을 입력하세요. : ", terminator: "")
+let myDay = Int(readLine() ?? "") ?? 0
+let myInterestRate = someInterestRate.getInterestRate(byDay: myDay)
+
+print("당신의 이자율은 \(myInterestRate*100)% 입니다.")
+
+print("당신의 원금을 입력하세요. : ", terminator: "")
+let myCalculatedAmount = someInterestRate.calculateAmount(day: myDay, amount: Int(readLine() ?? "") ?? 0)
+print("당신의 예치 계산 금액은 \(myCalculatedAmount)원 입니다.")
+
+print("100만원 예치시, 단위 기간에 따른 예치 계산을 확인하고 싶으시면 단위기간을 입력하세요. : " , terminator:  "")
+let myUnitDay = Int(readLine() ?? "") ?? 0
+
+for day in stride(from: 1, to: 365, by: myUnitDay) {
+    
+    print("\(day)일 : \(testInterest(unitDay: day))원")
+}
+print("365일 : \(testInterest(unitDay: 365))원")
