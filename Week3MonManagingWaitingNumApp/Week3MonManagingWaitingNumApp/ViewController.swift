aa0dfced4f4b6af202a6036f0a065c72bc9da70e
sol <hskim6521@naver.com>
2021-11-15 15:55:50 +0900

    Creat Week3 Monday Waiting Number Management App Project

---
+++ Week3MonManagingWaitingNumApp/Week3MonManagingWaitingNumApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week3MonManagingWaitingNumApp
+//
+//  Created by 김한솔 on 2021/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
